959c8e17dcb844d9d44a5969059318e802fe6c01
Austin Brock <austinjbrock@gmail.com>
2022-07-10 18:34:21 -0700

    added form variables and display functions

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
-//Books will be stored in an array 
-let myLibrary = [1,2,2,3,2,2,3];
+// //Books will be stored in an array 
+ let myLibrary = [2,5,4,6,7];
+// // const bookList = document.getElementById('bookList')
 
-const bookList = document.getElementById('bookList')
+//display Form will be hidden until clicked
+let displayFormBtn = document.getElementById('form-setup')
+let form = document.getElementById('form')
 
-function Book(){
+//add to library Btn
+let addToLib = document.createElement("button");
+addToLib.innerHTML += "Add To Library";
 
-}
+//Brings up the form onclick
+displayFormBtn.addEventListener('click',function() {
+    form.classList.remove('form')
+    document.body.appendChild(addToLib);
+});
 
-
+//adds book to current libraryArr
 function addBookToLibrary(){
 
    for (let i = 0; i < myLibrary.length; i++) {
-      const element = myLibrary[i];
-      console.log(element)
+      console.log(myLibrary[i]);
+
    }
+// bookList.textContent = myLibrary
+}
+addBookToLibrary()
 
 
-bookList.textContent = myLibrary
+function getFormData(){
+   let title = document.getElementById('form-title')
+   let author = document.getElementById('form-author')
+   let pages = document.getElementById('form-pages')
+   let isRead = document.getElementById('form-read')
 
+   console.log(title.value)
+   console.log(author)
+   console.log(pages)
+   console.log(isRead)
 }
 
-addBookToLibrary()
+
+getFormData()
